1317d5b12d613a243b1b9e6ca058171c87494066
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-21 09:59:40 +0100

    nah this aint it

--- RSADecryption.py
+++ RSADecryption.py
@@ -1,25 +1,31 @@
-def create_alphabet_library():
-    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
-    library = {}
-    for i, letter in enumerate(alphabet):
-        library[str(i + 1).zfill(2)] = letter
-    return library
 
 def split_string(string, n):
     return [string[i:i+n] for i in range(0, len(string), n)]
 
 def main():
     # Create Alphabet Library
-    alphabet_library = create_alphabet_library()
+    alphabet = 'abcdefghijklmnopqrstuvwxyz '
+    library = {}
+    for i, letter in enumerate(alphabet):
+        library[str(i)] = letter
 
     # Get Private Key
     PrivateKeyN = int(input("Enter n: "))
     PrivateKeyD = int(input("Enter d: "))
 
+    # Calculate the block size
+    k = 0
+    l = str(len(library))
+    while int(l) <= PrivateKeyN:
+        l += str(len(library))
+        k += 2
+        if int(l) > PrivateKeyN:
+            break
+
     # Get encoded message as list of ints
-    encodedMessage = [str(i).zfill(4) for i in input("Enter encoded message: ").split()]
-    if len(encodedMessage[0]) > 4:
-        encodedMessage = [int(encodedMessage[0][i:i+4]) for i in range(0, len(encodedMessage[0]), 4)]
+    encodedMessage = [str(i) for i in input("Enter encoded message: ").split()]
+    if len(encodedMessage[0]) > k:
+        encodedMessage = [int(encodedMessage[0][i:i+k]) for i in range(0, len(encodedMessage[0]), k)]
 
     decodedMessage = ""
     for i in encodedMessage:
@@ -32,7 +38,7 @@ def main():
         # Convert each pair to its corresponding letter using the alphabet library
         for pair in pairs:
             if pair != '00':
-                decodedMessage += alphabet_library[pair]
+                decodedMessage += library[pair]
             
 
     print(decodedMessage)
