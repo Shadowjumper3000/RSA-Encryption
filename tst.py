704650c466b76bc588b55cf45c953c9a04487838
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-21 14:43:54 +0100

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,8 +1,5 @@
 # idfk
-print((1379**1181) % 6059)
-i = 1379
+i = 94
 PrivateKeyD = 1181
 PrivateKeyN = 6059
 print(str(pow(int(i), PrivateKeyD, PrivateKeyN)))
-
-print(len("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "))
